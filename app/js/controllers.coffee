d6ff863a5f08c20a908d4a88fc65c17dc3c7ec4d
Nick Retallack <nickretallack@gmail.com>
2011-08-21 16:09:43 -0700

    prerequisites and postrequisites

--- app/js/controllers.coffee
+++ app/js/controllers.coffee
@@ -8,15 +8,21 @@ window.MyCtrl1 = ->
 
     # actions and aggregates
     @tasks_list = -> _.values(this.tasks)
+
+    @prerequisite_tasks_list = ->
+        return [] unless @something.current_task_id
+        @tasks[id] for id of @current_task.prerequisites
+
+    @postrequisite_tasks_list = ->
+        return [] unless @something.current_task_id
+        @tasks[id] for id of @current_task.postrequisites
+
     @remove = (task) -> 
         if @something.current_task_id is task.id 
             @something.current_task_id = null
         delete this.tasks[task.id]
     @add_task = ->
-        task = 
-            title:this.new_task_title
-            id:guidGenerator()
-        @tasks[task.id] = task
+        task = make_task(@new_task_title)
         @new_task_title = ''
     @save_tasks = _.debounce ->
         localStorage['tasks'] = JSON.stringify(this.tasks)
@@ -24,10 +30,39 @@ window.MyCtrl1 = ->
     @something.task_status = (task) ->
         if task.done then 'done' else 'new'
 
+    @add_prerequisite = ->
+        task = make_task(@new_prerequisite_task_title)
+        @add_relationship task, @current_task
+        @new_prerequisite_task_title = ''
+
+    @add_postrequisite = ->
+        task = make_task(@new_postrequisite_task_title)
+        @add_relationship @current_task, task
+        @new_postrequisite_task_title = ''
+
+    @add_relationship = (prerequisite, postrequisite) ->
+        postrequisite.prerequisites[prerequisite.id] = true
+        prerequisite.postrequisites[postrequisite.id] = true
+
+    @remove_relationship = (prerequisite, postrequisite) ->
+        delete postrequisite.prerequisites[prerequisite.id]
+        delete prerequisite.postrequisites[postrequisite.id]
+        console.debug "DOING IT!", prerequisite, postrequisite
+        
+    make_task = (title) =>
+        task = 
+            title:title
+            id:guidGenerator()
+            prerequisites:{}
+            postrequisites:{}
+        @tasks[task.id] = task
+        task
+        
+    @current_task = null
     # watchers
     this.$watch 'save_tasks()', ->
     this.$watch 'something.current_task_id', ->
-        this.current_task = this.tasks[this.something.current_task_id]
+        @current_task = this.tasks[@something.current_task_id]
 
 
 window.MyCtrl2 = ->
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,34 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   window.MyCtrl1 = function() {
-    var json_tasks;
+    var json_tasks, make_task;
     json_tasks = localStorage['tasks'];
     this.tasks = json_tasks ? JSON.parse(json_tasks) : {};
     this.something = {};
     this.tasks_list = function() {
       return _.values(this.tasks);
+    };
+    this.prerequisite_tasks_list = function() {
+      var id, _results;
+      if (!this.something.current_task_id) {
+        return [];
+      }
+      _results = [];
+      for (id in this.current_task.prerequisites) {
+        _results.push(this.tasks[id]);
+      }
+      return _results;
+    };
+    this.postrequisite_tasks_list = function() {
+      var id, _results;
+      if (!this.something.current_task_id) {
+        return [];
+      }
+      _results = [];
+      for (id in this.current_task.postrequisites) {
+        _results.push(this.tasks[id]);
+      }
+      return _results;
     };
     this.remove = function(task) {
       if (this.something.current_task_id === task.id) {
@@ -15,11 +38,7 @@
     };
     this.add_task = function() {
       var task;
-      task = {
-        title: this.new_task_title,
-        id: guidGenerator()
-      };
-      this.tasks[task.id] = task;
+      task = make_task(this.new_task_title);
       return this.new_task_title = '';
     };
     this.save_tasks = _.debounce(function() {
@@ -32,6 +51,39 @@
         return 'new';
       }
     };
+    this.add_prerequisite = function() {
+      var task;
+      task = make_task(this.new_prerequisite_task_title);
+      this.add_relationship(task, this.current_task);
+      return this.new_prerequisite_task_title = '';
+    };
+    this.add_postrequisite = function() {
+      var task;
+      task = make_task(this.new_postrequisite_task_title);
+      this.add_relationship(this.current_task, task);
+      return this.new_postrequisite_task_title = '';
+    };
+    this.add_relationship = function(prerequisite, postrequisite) {
+      postrequisite.prerequisites[prerequisite.id] = true;
+      return prerequisite.postrequisites[postrequisite.id] = true;
+    };
+    this.remove_relationship = function(prerequisite, postrequisite) {
+      delete postrequisite.prerequisites[prerequisite.id];
+      delete prerequisite.postrequisites[postrequisite.id];
+      return console.debug("DOING IT!", prerequisite, postrequisite);
+    };
+    make_task = __bind(function(title) {
+      var task;
+      task = {
+        title: title,
+        id: guidGenerator(),
+        prerequisites: {},
+        postrequisites: {}
+      };
+      this.tasks[task.id] = task;
+      return task;
+    }, this);
+    this.current_task = null;
     this.$watch('save_tasks()', function() {});
     return this.$watch('something.current_task_id', function() {
       return this.current_task = this.tasks[this.something.current_task_id];

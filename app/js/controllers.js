c4acf32d8083005d680f03cfa69902ca3a94141f
Nick Retallack <nickretallack@gmail.com>
2011-08-21 12:30:34 -0700

    switched from using an array to using a map for task storage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,21 @@
 function MyCtrl1() {
     var json_tasks = localStorage['tasks']
     if (json_tasks) this.tasks = JSON.parse(json_tasks)
-    else this.tasks = []
+    else this.tasks = {}
+    this.tasks_list = function(){
+        return _.values(this.tasks)
+    }
+    this.remove = function(task){
+        delete this.tasks[task.id]
+    }
 
     this.new_task_title = ''
     this.add_task = function(){
-        this.tasks.push({title:this.new_task_title, id:guidGenerator()})
+        var id = guidGenerator()
+        this.tasks[id] = {title:this.new_task_title, id:id}
         this.new_task_title = ''
     }
     this.something = {}
-    //this.something.current_task_id = this.tasks[0].id
-    /*this.current_task = function(){
-        return this.tasks[this.something.current_task_id]
-    }*/
 
     this.tasks_json = _.debounce(function(){
         localStorage['tasks'] = JSON.stringify(this.tasks)
@@ -24,14 +27,7 @@ function MyCtrl1() {
     this.$watch('tasks_json()', function(){})
 
     this.$watch('something.current_task_id', function(){
-        for (task in this.tasks){
-            task = this.tasks[task]
-            if (task.id === this.something.current_task_id) {
-                this.current_task = task
-                this.$eval()
-                break
-            }
-        }
+        this.current_task = this.tasks[this.something.current_task_id]
     })
 }
 //MyCtrl1.$inject = [];
